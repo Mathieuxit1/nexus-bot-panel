8535ebbf9bcb6fcac30cea0240d474e104d4bc1d
Mathieuxit <tom.noirot21000@gmail.com>
2025-11-16 19:46:46 +0100

    remoove

--- frontend/script.js
+++ frontend/script.js
@@ -18,7 +18,7 @@ document.getElementById("login-btn").addEventListener("click", () => {
   loader.classList.remove("hidden");
 
   setTimeout(() => {
-    loader.classList.add("hidden");
+    loader.classList.add("");
 
     const clientId = "1382943232966529064";
     const redirectUri = "https://site-peach-omega.vercel.app/callback";
